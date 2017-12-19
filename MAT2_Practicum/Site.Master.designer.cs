51c1e1a5bbc75288702a5c1d8f0177901fd26173
humphric <34656273+humphric@users.noreply.github.com>
2017-12-19 08:50:40 -0500

    Added Bootstrap and Modifyed the Site Master

--- MAT2_Practicum/Site.Master.designer.cs
+++ MAT2_Practicum/Site.Master.designer.cs
@@ -7,22 +7,20 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MAT2_Practicum
-{
-
-
-    public partial class SiteMaster
-    {
-
+namespace MAT2_Practicum {
+    
+    
+    public partial class SiteMaster {
+        
         /// <summary>
-        /// HeadContent control.
+        /// HeaderContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
-
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeaderContent;
+        
         /// <summary>
         /// MainContent control.
         /// </summary>
